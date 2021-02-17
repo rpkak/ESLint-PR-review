5afc2dbb454167240a1fd40d32f4fd935cd397f0
rpkak <arne@rpkak.de>
2021-02-17 11:09:29 +0100

    pipe test deou

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-debug() {
+deou() {
     while read -r data; do
         echo "::debug::$data"
     done
 }
 
-echo  "Install ESLint-PR-review Packages:" | debug
-npm ci --prefix /ESLint-PR-review | debug
-echo | debug
+echo  "Install ESLint-PR-review Packages:" | deou
+npm ci --prefix /ESLint-PR-review | deou
+echo | deou
 
-echo "Install Project Packages:" | debug
-npm ci --prefix $1 | debug
-echo | debug
+echo "Install Project Packages:" | deou
+npm ci --prefix $1 | deou
+echo | deou
 
 
