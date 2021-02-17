2e5f15d4c74b6f7f368d5b638bf650fe6e2730c6
rpkak <arne@rpkak.de>
2021-02-17 13:07:06 +0100

    ls

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,4 +18,5 @@ npm run package --prefix /ESLint-PR-review | debug
 echo "Install Project Packages:" | debug
 npm ci --prefix $1 | debug
 
+ls /ESLint-PR-review
 node /ESLint-PR-review/lib/main.js $1 $2 $3 $4
