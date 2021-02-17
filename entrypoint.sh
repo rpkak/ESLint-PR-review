1617c15349cafda7cbf2b5a1c33b6c1cdc2c9538
rpkak <arne@rpkak.de>
2021-02-17 10:29:07 +0100

    $1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# echo "::debug::Install ESLint-PR-review Packages:"
-# echo "::debug::$(npm ci --prefix /ESLint-PR-review)"
-# echo "::debug::"
+echo "::debug::Install ESLint-PR-review Packages:"
+echo "::debug::$(npm ci --prefix /ESLint-PR-review)"
+echo "::debug::"
 
-# echo "::debug::Install Project Packages:"
-# echo "::debug::$(npm ci --prefix $INPUT_PROJECT-)"
-# echo "::debug::"
-echo "$INPUT_PROJECT-ROOT"
+echo "::debug::Install Project Packages:"
+echo "::debug::$(npm ci --prefix $1)"
+echo "::debug::"
