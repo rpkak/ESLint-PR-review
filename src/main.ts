fe1127f71546c43c913acc8bb42a8b498e986af9
rpkak <arne@rpkak.de>
2021-02-20 16:20:00 +0100

    much

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import * as core from '@actions/core'
-import { context, getOctokit } from '@actions/github'
-import { ESLint } from 'eslint'
-import { readFileSync } from 'fs'
-import { isAbsolute, join } from 'path'
-import { argv } from 'process'
+import {context, getOctokit} from '@actions/github'
+import {ESLint} from 'eslint'
+import {readFileSync} from 'fs'
+import {isAbsolute, join} from 'path'
+import {argv} from 'process'
 
 const run = async (): Promise<void> => {
   try {
@@ -20,88 +20,125 @@ const run = async (): Promise<void> => {
     if (context.eventName === 'pull_request') {
       const octokit = getOctokit(argv[4])
 
-      core.info(JSON.stringify(
+      const filesChanged = (
         await octokit.pulls.listFiles({
           ...context.repo,
           pull_number: context.payload.pull_request?.number as number
-        }))
-      )
+        })
+      ).data.map(file => file.filename)
 
-      // const comments = []
-      // for (const file of resultArr) {
-      //   for (const message of file.messages) {
-      //     if (message.fix) {
-      //       const normalFileContent = readFileSync(file.filePath).toString()
-      //       const normalLines = normalFileContent.split('\n')
-      //       const fixedFileContent =
-      //         normalFileContent.substr(0, message.fix.range[0]) +
-      //         message.fix.text +
-      //         normalFileContent.substr(message.fix.range[1])
-      //       const fixedLines = fixedFileContent.split('\n')
-      //       let startLine = 0
-      //       while (normalLines[startLine] === fixedLines[startLine]) {
-      //         startLine++
-      //       }
-      //       const difference = normalLines.length - fixedLines.length
-      //       let line = normalLines.length
-      //       while (normalLines[line] === fixedLines[line - difference]) {
-      //         line--
-      //       }
+      const allComments: {
+        path: string
+        body: string
+        start_line?: number
+        line: number
+      }[] = []
+      for (const file of resultArr) {
+        for (const message of file.messages) {
+          if (message.fix) {
+            const normalFileContent = readFileSync(file.filePath).toString()
+            const normalLines = normalFileContent.split('\n')
+            const fixedFileContent =
+              normalFileContent.substr(0, message.fix.range[0]) +
+              message.fix.text +
+              normalFileContent.substr(message.fix.range[1])
+            const fixedLines = fixedFileContent.split('\n')
+            let startLine = 0
+            while (normalLines[startLine] === fixedLines[startLine]) {
+              startLine++
+            }
+            const difference = normalLines.length - fixedLines.length
+            let line = normalLines.length
+            while (normalLines[line] === fixedLines[line - difference]) {
+              line--
+            }
 
-      //       const newLines = fixedLines.slice(startLine, line - difference + 1)
+            const newLines = fixedLines.slice(startLine, line - difference + 1)
 
-      //       startLine++
-      //       line++
-      //       comments.push({
-      //         path: file.filePath.replace(`${process.cwd()}/`, '/'),
-      //         body: `${message.message}\n\`\`\`suggestion\n${newLines.join(
-      //           '\n'
-      //         )}\n\`\`\``,
-      //         start_line: startLine === line ? undefined : startLine,
-      //         line
-      //       })
-      //     } else {
-      //       comments.push({
-      //         path: file.filePath.replace(`${process.cwd()}/`, '/'),
-      //         body: message.message,
-      //         start_line:
-      //           message.line === message.endLine ? undefined : message.line,
-      //         line: message.endLine
-      //       })
-      //     }
-      //   }
-      // }
-      // console.log(comments)
+            startLine++
+            line++
+            allComments.push({
+              path: file.filePath.replace(`${process.cwd()}/`, ''),
+              body: `${message.message}\n\`\`\`suggestion\n${newLines.join(
+                '\n'
+              )}\n\`\`\``,
+              start_line: startLine === line ? undefined : startLine,
+              line
+            })
+          } else {
+            allComments.push({
+              path: file.filePath.replace(`${process.cwd()}/`, ''),
+              body: message.message,
+              start_line:
+                message.line === message.endLine ? undefined : message.line,
+              line: message.endLine as number
+            })
+          }
+        }
+      }
+      console.log(allComments)
+      const comments = []
+      const bodyComments = new Map<
+        string,
+        {
+          path: string
+          body: string
+          start_line?: number
+          line: number
+        }[]
+      >()
 
-      // for (const comment of comments) {
-      //   const reviewComment = await octokit.pulls.createReviewComment({
-      //     ...context.repo,
-      //     pull_number: context.payload.pull_request?.number as number,
-      //     ...comment
-      //   })
-      // }
+      for (const comment of allComments) {
+        if (filesChanged.includes(comment.path)) {
+          comments.push(comment)
+        } else {
+          if (bodyComments.has(comment.path)) {
+            bodyComments.get(comment.path)?.push(comment)
+          } else {
+            bodyComments.set(comment.path, [comment])
+          }
+        }
+      }
 
-      // const review = await octokit.pulls.createReview({
-      //   ...context.repo,
-      //   pull_number: context.payload.pull_request?.number as number,
-      //   body: comments.length
-      //     ? `## ${comments.length} Problems found`
-      //     : undefined,
-      //   comments,
-      //   headers: {
-      //     accept: 'application/vnd.github.v3+json'
-      //   }
-      // })
-      // await octokit.pulls.submitReview({
-      //   ...context.repo,
-      //   event: comments.length ? 'REQUEST_CHANGES' : 'APPROVE',
-      //   pull_number: context.payload.pull_request?.number as number,
-      //   review_id: review.data.id
-      // })
-      if (comments.length) {
+      const review = await octokit.pulls.createReview({
+        ...context.repo,
+        pull_number: context.payload.pull_request?.number as number,
+        body: allComments.length
+          ? `## ${allComments.length} Problems found${
+              bodyComments.size
+                ? `\n${[...bodyComments.entries()]
+                    .map(
+                      ([path, commentArr]) =>
+                        `### ${path}\n${commentArr
+                          .map(
+                            comment =>
+                              `${
+                                comment.start_line === undefined
+                                  ? `Line ${comment.line}:`
+                                  : `From line ${comment.start_line} to ${comment.line}:`
+                              }\n${comment.body}`
+                          )
+                          .join('\n---\n')}`
+                    )
+                    .join('\n')}`
+                : ''
+            }`
+          : undefined,
+        comments,
+        headers: {
+          accept: 'application/vnd.github.v3+json'
+        }
+      })
+      await octokit.pulls.submitReview({
+        ...context.repo,
+        event: allComments.length ? 'REQUEST_CHANGES' : 'APPROVE',
+        pull_number: context.payload.pull_request?.number as number,
+        review_id: review.data.id
+      })
+      if (allComments.length) {
         const formatter = await eslint.loadFormatter(argv[5])
         const formatted = formatter.format(resultArr)
-        console.log("pr")
+        console.log('pr')
         core.setFailed(formatted)
       }
     } else {
@@ -113,7 +150,7 @@ const run = async (): Promise<void> => {
       ) {
         const formatter = await eslint.loadFormatter(argv[5])
         const formatted = formatter.format(resultArr)
-        console.log("not pr")
+        console.log('not pr')
         core.setFailed(formatted)
       }
     }
