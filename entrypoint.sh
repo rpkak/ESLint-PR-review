67e03a712422dc91bc09735ac258e15434146f41
rpkak <arne@rpkak.de>
2021-02-20 07:53:34 +0100

    remove unused

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,10 +12,4 @@ npm ci --prefix /ESLint-PR-review | debug
 echo "Build ESLint-PR-review:" | debug
 npm run build --prefix /ESLint-PR-review | debug
 
-echo "Build ESLint-PR-review:" | debug
-npm run package --prefix /ESLint-PR-review | debug
-
-echo "Install Project Packages:" | debug
-npm ci --prefix $1 | debug
-
 node /ESLint-PR-review/lib/main.js $1 $2 $3 $4
