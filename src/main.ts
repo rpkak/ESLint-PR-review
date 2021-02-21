0c9b34a173bde4c23d63cef4a05d9e34850e700e
rpkak <arne@rpkak.de>
2021-02-21 08:19:26 +0100

    list

--- src/main.ts
+++ src/main.ts
@@ -19,125 +19,134 @@ const run = async (): Promise<void> => {
     if (context.eventName === 'pull_request') {
       const octokit = getOctokit(argv[4])
 
-      const filesChanged = (
-        await octokit.pulls.listFiles({
-          ...context.repo,
-          pull_number: context.payload.pull_request?.number as number
-        })
-      ).data.map(file => file.filename)
+      core.debug(
+        JSON.stringify(
+          await octokit.pulls.listReviews({
+            ...context.repo,
+            pull_number: context.payload.pull_request?.number as number
+          })
+        )
+      )
 
-      const allComments: {
-        path: string
-        body: string
-        start_line?: number
-        line: number
-      }[] = []
-      for (const file of resultArr) {
-        for (const message of file.messages) {
-          if (message.fix) {
-            const normalFileContent = readFileSync(file.filePath).toString()
-            const normalLines = normalFileContent.split('\n')
-            const fixedFileContent =
-              normalFileContent.substr(0, message.fix.range[0]) +
-              message.fix.text +
-              normalFileContent.substr(message.fix.range[1])
-            const fixedLines = fixedFileContent.split('\n')
-            let startLine = 0
-            while (normalLines[startLine] === fixedLines[startLine]) {
-              startLine++
-            }
-            const difference = normalLines.length - fixedLines.length
-            let line = normalLines.length
-            while (normalLines[line] === fixedLines[line - difference]) {
-              line--
-            }
+      // const filesChanged = (
+      //   await octokit.pulls.listFiles({
+      //     ...context.repo,
+      //     pull_number: context.payload.pull_request?.number as number
+      //   })
+      // ).data.map(file => file.filename)
 
-            const newLines = fixedLines.slice(startLine, line - difference + 1)
+      // const allComments: {
+      //   path: string
+      //   body: string
+      //   start_line?: number
+      //   line: number
+      // }[] = []
+      // for (const file of resultArr) {
+      //   for (const message of file.messages) {
+      //     if (message.fix) {
+      //       const normalFileContent = readFileSync(file.filePath).toString()
+      //       const normalLines = normalFileContent.split('\n')
+      //       const fixedFileContent =
+      //         normalFileContent.substr(0, message.fix.range[0]) +
+      //         message.fix.text +
+      //         normalFileContent.substr(message.fix.range[1])
+      //       const fixedLines = fixedFileContent.split('\n')
+      //       let startLine = 0
+      //       while (normalLines[startLine] === fixedLines[startLine]) {
+      //         startLine++
+      //       }
+      //       const difference = normalLines.length - fixedLines.length
+      //       let line = normalLines.length
+      //       while (normalLines[line] === fixedLines[line - difference]) {
+      //         line--
+      //       }
 
-            startLine++
-            line++
-            allComments.push({
-              path: file.filePath.replace(`${process.cwd()}/`, ''),
-              body: `${message.message}\n\`\`\`suggestion\n${newLines.join(
-                '\n'
-              )}\n\`\`\``,
-              start_line: startLine === line ? undefined : startLine,
-              line
-            })
-          } else {
-            allComments.push({
-              path: file.filePath.replace(`${process.cwd()}/`, ''),
-              body: message.message,
-              start_line:
-                message.line === message.endLine ? undefined : message.line,
-              line: message.endLine as number
-            })
-          }
-        }
-      }
-      const comments = []
-      const bodyComments = new Map<
-        string,
-        {
-          path: string
-          body: string
-          start_line?: number
-          line: number
-        }[]
-      >()
+      //       const newLines = fixedLines.slice(startLine, line - difference + 1)
 
-      for (const comment of allComments) {
-        if (filesChanged.includes(comment.path)) {
-          comments.push(comment)
-        } else {
-          if (bodyComments.has(comment.path)) {
-            bodyComments.get(comment.path)?.push(comment)
-          } else {
-            bodyComments.set(comment.path, [comment])
-          }
-        }
-      }
+      //       startLine++
+      //       line++
+      //       allComments.push({
+      //         path: file.filePath.replace(`${process.cwd()}/`, ''),
+      //         body: `${message.message}\n\`\`\`suggestion\n${newLines.join(
+      //           '\n'
+      //         )}\n\`\`\``,
+      //         start_line: startLine === line ? undefined : startLine,
+      //         line
+      //       })
+      //     } else {
+      //       allComments.push({
+      //         path: file.filePath.replace(`${process.cwd()}/`, ''),
+      //         body: message.message,
+      //         start_line:
+      //           message.line === message.endLine ? undefined : message.line,
+      //         line: message.endLine as number
+      //       })
+      //     }
+      //   }
+      // }
+      // const comments = []
+      // const bodyComments = new Map<
+      //   string,
+      //   {
+      //     path: string
+      //     body: string
+      //     start_line?: number
+      //     line: number
+      //   }[]
+      // >()
 
-      const review = await octokit.pulls.createReview({
-        ...context.repo,
-        pull_number: context.payload.pull_request?.number as number,
-        body: allComments.length
-          ? `## ${allComments.length} Problems found${
-              bodyComments.size
-                ? `\n${[...bodyComments.entries()]
-                    .map(
-                      ([path, commentArr]) =>
-                        `### ${path}\n${commentArr
-                          .map(
-                            comment =>
-                              `${
-                                comment.start_line === undefined
-                                  ? `Line ${comment.line}:`
-                                  : `From line ${comment.start_line} to ${comment.line}:`
-                              }\n${comment.body}`
-                          )
-                          .join('\n---\n')}`
-                    )
-                    .join('\n')}`
-                : ''
-            }`
-          : undefined,
-        comments,
-        headers: {
-          accept: 'application/vnd.github.v3+json'
-        }
-      })
-      await octokit.pulls.submitReview({
-        ...context.repo,
-        event: allComments.length ? 'REQUEST_CHANGES' : 'APPROVE',
-        pull_number: context.payload.pull_request?.number as number,
-        review_id: review.data.id
-      })
-      if (allComments.length) {
-        const formatter = await eslint.loadFormatter(argv[5])
-        const formatted = formatter.format(resultArr)
-        core.setFailed(formatted)
-      }
+      // for (const comment of allComments) {
+      //   if (filesChanged.includes(comment.path)) {
+      //     comments.push(comment)
+      //   } else {
+      //     if (bodyComments.has(comment.path)) {
+      //       bodyComments.get(comment.path)?.push(comment)
+      //     } else {
+      //       bodyComments.set(comment.path, [comment])
+      //     }
+      //   }
+      // }
+
+      // const review = await octokit.pulls.createReview({
+      //   ...context.repo,
+      //   pull_number: context.payload.pull_request?.number as number,
+      //   body: allComments.length
+      //     ? `## ${allComments.length} Problems found${
+      //         bodyComments.size
+      //           ? `\n${[...bodyComments.entries()]
+      //               .map(
+      //                 ([path, commentArr]) =>
+      //                   `### ${path}\n${commentArr
+      //                     .map(
+      //                       comment =>
+      //                         `${
+      //                           comment.start_line === undefined
+      //                             ? `Line ${comment.line}:`
+      //                             : `From line ${comment.start_line} to ${comment.line}:`
+      //                         }\n${comment.body}`
+      //                     )
+      //                     .join('\n---\n')}`
+      //               )
+      //               .join('\n')}`
+      //           : ''
+      //       }`
+      //     : undefined,
+      //   comments,
+      //   headers: {
+      //     accept: 'application/vnd.github.v3+json'
+      //   }
+      // })
+      // await octokit.pulls.submitReview({
+      //   ...context.repo,
+      //   event: allComments.length ? 'REQUEST_CHANGES' : 'APPROVE',
+      //   pull_number: context.payload.pull_request?.number as number,
+      //   review_id: review.data.id
+      // })
+      // if (allComments.length) {
+      //   const formatter = await eslint.loadFormatter(argv[5])
+      //   const formatted = formatter.format(resultArr)
+      //   core.setFailed(formatted)
+      // }
     } else {
       if (
         resultArr.reduce(
