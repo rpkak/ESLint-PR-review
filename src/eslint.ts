c7b7dc30853077fe4ce7b8f57c50ff4f57892075
rpkak <arne@rpkak.de>
2021-03-13 08:14:37 +0100

    add extensions to ESLint

--- src/eslint.ts
+++ src/eslint.ts
@@ -2,7 +2,9 @@ import {argv} from 'process'
 import {ESLint} from 'eslint'
 
 const run = async (): Promise<void> => {
-  const eslint = new ESLint({})
+  const eslint = new ESLint({
+    extensions: argv[4].split(',')
+  })
 
   const resultArr = await eslint.lintFiles(argv[2])
   const formatter = await eslint.loadFormatter(argv[3])
