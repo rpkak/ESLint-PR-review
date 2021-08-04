16bf9215ae2e2b2f8772de7d5905d61b5123c503
rpkak <rpkak@users.noreply.github.com>
2021-08-04 10:45:03 +0200

    catch eslint errors

--- src/eslint.ts
+++ src/eslint.ts
@@ -1,21 +1,26 @@
 import {argv} from 'process'
 import {ESLint} from 'eslint'
+import * as core from '@actions/core'
 
 const run = async (): Promise<void> => {
-  const eslint = new ESLint({
-    extensions: argv[4].split(',')
-  })
-
-  const resultArr = await eslint.lintFiles(argv[2])
-  const formatter = await eslint.loadFormatter(argv[3])
-  const formatted = formatter.format(resultArr)
-  // eslint-disable-next-line no-console
-  console.log(
-    JSON.stringify({
-      resultArr,
-      formatted
+  try {
+    const eslint = new ESLint({
+      extensions: argv[4].split(',')
     })
-  )
+
+    const resultArr = await eslint.lintFiles(argv[2])
+    const formatter = await eslint.loadFormatter(argv[3])
+    const formatted = formatter.format(resultArr)
+    // eslint-disable-next-line no-console
+    console.log(
+      JSON.stringify({
+        resultArr,
+        formatted
+      })
+    )
+  } catch (error) {
+    core.setFailed(error.stack)
+  }
 }
 
 run()
